e56906b153ad2c14b622c04055de6eb1aadcbd3f
Dmitry.Vasilyev <dmitry.vasilyev@trs-it.ru>
2016-02-02 19:58:55 +0300

    rational

---
+++ test-scala/src/main/scala/test/Rational.scala
@@ -0,0 +1,10 @@
+package test
+
+/**
+ * @author Dmitry Vasilyev.
+ *         http://www.otstavnov.com/Odersky_Scala_lecture-notes.html
+ */
+class Rational(x: Int, y: Int) {
+  def numer = x
+  def denom = y
+}
